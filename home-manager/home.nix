72e2a0a5df391e2c029958bc54ba48679f2755e0
SailorSnoW <sailorsnow@pm.me>
2024-11-27 01:42:00 +0100

    use outputs instead of raw paths

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -4,15 +4,7 @@
 }: {
   # You can import other home-manager modules here
   imports = [
-    # If you want to use modules your own flake exports (from modules/home-manager):
-    # outputs.homeManagerModules.example
-
-    # Or modules exported from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModules.default
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
-    ../modules/home-manager/zsh.nix
+    outputs.homeManagerModules.zsh
   ];
 
   nixpkgs = {
