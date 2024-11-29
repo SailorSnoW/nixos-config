f0a0740160fb4cc2388754815271d643e5c0a105
SailorSnoW <sailorsnow@pm.me>
2024-11-29 13:03:46 +0100

    add virtual hosts

--- modules/homelab/reverse-proxy.nix
+++ modules/homelab/reverse-proxy.nix
@@ -3,8 +3,19 @@
 
   services.caddy = {
     enable = true;
-    virtualHosts."localhost".extraConfig = ''
-      respond "Hello, world!"
+    virtualHosts."julianlagoutte.fr".extraConfig = ''
+      reverse_proxy 100.115.252.77:8080
+    '';
+    virtualHosts."www.julianlagoutte.fr".extraConfig = ''
+      reverse_proxy 100.115.252.77:8080
+    '';
+
+    # Pterodactyl related
+    virtualHosts."panel.snxw.moe".extraConfig = ''
+      reverse_proxy 100.65.121.117:8081
+    '';
+    virtualHosts."wings1.snxw.moe".extraConfig = ''
+      reverse_proxy 192.168.1.54:443
     '';
   };
 }
