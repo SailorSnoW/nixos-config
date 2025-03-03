52fc2ee5627732342dfb4077e27486c831fd57ef
SailorSnoW <sailorsnow@pm.me>
2025-03-03 11:14:06 +0100

    remove roon overlay as not needed anymore

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,18 +6,7 @@
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
-  modifications = final: prev: {
-    roon-server = prev.roon-server.overrideAttrs (oldAttrs: rec {
-    version = "2.0-1483";
-
-    urlVersion = builtins.replaceStrings [ "." "-" ] [ "00" "0" ] version;
-
-    src = prev.fetchurl {
-      url = "https://download.roonlabs.com/updates/production/RoonServer_linuxx64_${urlVersion}.tar.bz2";
-      hash = "sha256-y8MYiWlc3HfF7a3n7yrs84H/9KbEoANd8+7t2ORIm6w="; 
-    };
-  });
-  };
+  modifications = final: prev: {};
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.unstable'
