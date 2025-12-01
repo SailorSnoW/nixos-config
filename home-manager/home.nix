02ec2a851acbc911102954e43d6cfa8e71190926
SailorSnoW <sailorsnow@pm.me>
2025-12-01 15:05:33 +0100

    fix mesa firefox and system variable warning

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -38,6 +38,7 @@
     age
     devenv
     claude-code
+    lazydocker
   ];
 
   programs.direnv = {
--- hosts/asahi/configuration.nix
+++ hosts/asahi/configuration.nix
@@ -53,6 +53,15 @@
     };
   };
 
+  hardware.graphics.package =
+    # FIXME: Workaround for Mesa 25.3.0 regression
+    # https://github.com/nix-community/nixos-apple-silicon/issues/380
+    assert pkgs.mesa.version == "25.3.0";
+    (import (fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/c5ae371f1a6a7fd27823bc500d9390b38c05fa55.tar.gz";
+      sha256 = "sha256-4PqRErxfe+2toFJFgcRKZ0UI9NSIOJa+7RXVtBhy4KE=";
+    }) { localSystem = pkgs.stdenv.hostPlatform; }).mesa;
+
   powerManagement = {
     enable = true;
   };
@@ -88,7 +97,7 @@
     podman-tui
     podman-compose
     light
-    inputs.noctalia.packages.${system}.default
+    inputs.noctalia.packages.${stdenv.hostPlatform.system}.default
   ];
 
   services = {
